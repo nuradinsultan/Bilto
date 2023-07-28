3ace6ba99fa7050148e59d57ac17f7a6e4f01a50
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-07-28 15:29:37 +0300

    Update routes.py

--- App/routes.py
+++ App/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return ""
+    user = {'username': ''}
+    return render_template('base.html', title='Home', user=user ""
