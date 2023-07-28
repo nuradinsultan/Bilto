8c05c32f91c16591d9e0cf69d621fa7442217a66
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-07-28 18:34:57 +0300

    Update routes.py

--- App/routes.py
+++ App/routes.py
@@ -4,10 +4,15 @@ from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def index():
     user = {'username': ''}
     return render_template('base.html', title='Home', user=user ""
+
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html
