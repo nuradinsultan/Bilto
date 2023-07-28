23213343aa902f2c41d7a7bc47d254a808e79673
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-07-28 19:58:30 +0300

    Update models.py

--- App/models.py
+++ App/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from app import login
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +17,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+        
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,3 +27,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+        
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
