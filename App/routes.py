f453c51268afa3f525117bda6e310286dc3b8009
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-07-28 20:33:55 +0300

    Update routes.py

--- App/routes.py
+++ App/routes.py
@@ -4,6 +4,8 @@ from app.forms import LoginForm
 from flask_login import current_user, login_user
 from app.models import User
 from flask_login import logout_user
+from flask import request
+from werkzeug.urls import url_parse
 from flask_login import login_required
 
 @app.route('/')
@@ -21,6 +23,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
 @app.route('/logout')
 def logout():
     logout_user()
