530c996d8f892c3d7d38bb110675bca08b14ee7e
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-07-28 20:17:32 +0300

    Update __init__.py

--- App/__init__.py
+++ App/__init__.py
@@ -7,5 +7,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
+login.login_view = 'login'
