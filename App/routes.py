92a6b82ff5bc8117e4d11012503d3654736f0d38
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-07-28 18:18:40 +0300

    Update routes.py

--- App/routes.py
+++ App/routes.py
@@ -1,8 +1,13 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+@app.route('/login')
 def index():
     user = {'username': ''}
     return render_template('base.html', title='Home', user=user ""
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
