79aaba56b5d039b69013826c21590585bed15bd4
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-07-29 18:19:37 +0300

    Update models.py

--- App/models.py
+++ App/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from app import db
-from werkzeug.security import generate_password_hash, check_password_hash
-from app import login
 from hashlib import md5
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,7 +19,8 @@ class User(UserMixin, db.Model):
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')                  
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')   
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -44,6 +45,11 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -53,6 +59,4 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
         
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+
